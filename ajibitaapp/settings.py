a1cfe9cee3c3fec429ec597a3f59dff56290dec9
fabio.neves <fabio.miguel.neves@gmail.com>
2012-10-21 17:21:50 +0100

    changed settings file

--- ajibitaapp/settings.py
+++ ajibitaapp/settings.py
@@ -164,4 +164,5 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
