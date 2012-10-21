0413db5fd6b6ab7b2ca4609688e0a4fc668c8b34
fabio.neves <fabio.miguel.neves@gmail.com>
2012-10-21 16:02:42 +0100

    changed settings file

--- ajibitaapp/settings.py
+++ ajibitaapp/settings.py
@@ -10,17 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'default',                      # Or path to database file if using sqlite3.
-        'USER': 'fabio',                      # Not used with sqlite3.
-        'PASSWORD': 'fabio',                  # Not used with sqlite3.
-        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -151,3 +140,18 @@ LOGGING = {
         },
     }
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'default',                      # Or path to database file if using sqlite3.
+        'USER': 'fabio',                      # Not used with sqlite3.
+        'PASSWORD': 'fabio',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+import dj_database_url
+#commment on development time
+DATABASES['default'] =  dj_database_url.config()
